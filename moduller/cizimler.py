ab4f1980e3a3c1ae7f09bdc397ef3a4d4f2f97c2
Muhammet925 <kaplansavasci@gmail.com>
2025-12-20 10:45:58 +0300

    deneme4

---
+++ moduller/cizimler.py
@@ -0,0 +1,36 @@
+def cizimmenu():
+    print("Ana Menü")
+    print("\033[1;32;40m")
+    #print("╔"+"═"*20+"╗")
+    print("╔═════════════════════╗")
+    print("║\033[1;31;40m    çizimlermenu   \033[1;32;40m     ║")
+    print("║                     ║")
+    print("║  1-KARE             ║")
+    print("║  2-Üçgen            ║")
+    print("║  3-desen            ║")
+    print("║  4-pikachu          ║")
+    print("║  5-kaplan           ║")
+    print("║    Seçimiz nedir?   ║")
+    print("╚═════════════════════╝")
+    # 201 ╔ # 205 ═ # 187 ╗ # 186 ║ # 200 ╚ # 188 ╝
+    secim=input("Seçiminiz:")
+    if secim=="1" or secim=="KARE":
+        print("KARE seçtiniz.")
+        import moduller.cizimler_kare        
+    elif secim=="2" or secim=="Üçgen":
+        print("Üçgen seçtiniz.")
+    elif secim=="3" or secim=="desen":
+        print("Desen seçtiniz.")
+    elif secim=="4" or secim=="pikachu":
+        print("Pikachu seçtiniz.")
+    elif secim=="5" or secim=="kaplan":
+        print("Kaplan seçtiniz.")
+    elif secim=="6" or secim=="çıkış":
+        print("Çıkış yapıldı.")
+        import sys  
+        sys.exit()
+    else:
+        print("Geçersiz seçim.")    
+    # cizimmenu()
+
+# cizimmenu()  
---
+++ moduller/cizimler_kare.py
@@ -0,0 +1,7 @@
+import turtle
+for a in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+input("Sonraki kenar için Enter'a basın...")
+   
+    
